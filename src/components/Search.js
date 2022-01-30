519bb7d22b6903b5e8b46888d712d5ff1ce5ac64
ZachCodedThat <Zachemailsback@gmail.com>
2022-01-29 23:55:24 -0500

    got all logic around pagination lined up

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,32 +1,36 @@
-import React, { useReducer, useState, useEffect } from "react";
-import { NextRouter } from "next/dist/client/router";
+import { useState, useEffect } from "react";
+
 import useDebounce from "@utils/useDebounce";
 
 const Search = () => {
   const [user, setUser] = useState([]);
-  const [userQuery, setUserQuery] = useState("");
   const [query, setQuery] = useState("");
   const [page, setPage] = useState(1);
   const [total, setTotal] = useState();
 
   const debouncedQuery = useDebounce(query, 500);
 
-  const pageTotal = Math.ceil(total / 5);
-
-  //   const handleQuery = (e) => {
-  //     e.preventDefault();
-  //     setQuery(userQuery);
-  //     setPage(1);
-  //   };
+  const pageTotal = Math.ceil(total / 10);
 
   const incrementPage = () => {
     if (page < pageTotal) {
-      setPage(page + 1);
+      setPage(++page);
     }
   };
 
   const decrementPage = () => {
-    setPage(page - 1);
+    setPage(--page);
+  };
+
+  const setPagetoMax = () => {
+    setPage(pageTotal);
+  };
+
+  const handleUpperLimit = (e) => {
+    if (page > pageTotal) {
+      setPage(pageTotal);
+    }
+    setPage(e.target.value);
   };
 
   const getUser = async () => {
@@ -36,6 +40,7 @@ const Search = () => {
     });
     const data = await res.json();
     console.log(data);
+
     setUser(data.items);
     setTotal(data.total_count);
   };
@@ -55,23 +60,43 @@ const Search = () => {
             There are {total} hits for that user on {pageTotal} pages
           </h2>
           <h2>You are on page {page}</h2>
+          <span>Jump to page # </span>
+          <input
+            type="number"
+            value={page}
+            onChange={handleUpperLimit}
+            min="1"
+            max={pageTotal}
+          />
         </div>
       ) : (
         <h2>No results</h2>
       )}
 
       <div>
-        {user && page != 1 && <button onClick={decrementPage}>Previous</button>}
-        {user && page != pageTotal && (
-          <button onClick={incrementPage}>Next</button>
+        {page > pageTotal ? (
+          <button onClick={setPagetoMax}>Take me back to the last page</button>
+        ) : (
+          <div>
+            {user && page != 1 && (
+              <button onClick={decrementPage}>Previous</button>
+            )}
+            {user && page < pageTotal && (
+              <button onClick={incrementPage}>Next</button>
+            )}
+          </div>
         )}
+
         {user &&
           user.map((user) => (
             <>
               <div key={user.id}>
-                {user.login} {user.id}
+                {user.login}
+                <br />
+                {user.id}
               </div>
               <img src={user.avatar_url} />
+
               <a href={user.html_url} target="_blank" rel="noreferrer">
                 Check them out on GitHub!
               </a>
@@ -81,9 +106,5 @@ const Search = () => {
     </>
   );
 };
-
-export async function getStaticProps() {
-  return {};
-}
 
 export default Search;
--- src/utils/useDebounce.js
+++ src/utils/useDebounce.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function useDebounce(value, delay) {
   // State and setters for debounced value
